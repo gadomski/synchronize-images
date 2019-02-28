5387043c253be4baa0606456e50777fc659e214d
Pete Gadomski <pete.gadomski@gmail.com>
2019-02-28 08:46:26 -0700

    Set up synchro parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate chrono;
 #[macro_use]
 extern crate failure;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 
 use chrono::{DateTime, Utc};
 use std::path::Path;
@@ -12,8 +15,10 @@ fn main() {
 
 /// The errors that can be produced by this executable.
 #[derive(Debug, Fail)]
-#[fail(display = "generic error")]
-struct Error {}
+enum Error {
+    #[fail(display = "this string could not be parsed as an event marker: {}", _0)]
+    InvalidEventMarker(String),
+}
 
 /// A "SYNCRO" file from Apps.
 ///
@@ -41,10 +46,15 @@ impl Synchro {
         let reader = BufReader::new(File::open(path)?);
         let event_markers = reader
             .lines()
-            .map(|result| {
-                result
-                    .map_err(failure::Error::from)
-                    .and_then(|line| line.parse().map_err(failure::Error::from))
+            .filter_map(|result| match result {
+                Ok(line) => {
+                    if line.is_empty() || line.starts_with('#') {
+                        None
+                    } else {
+                        Some(line.parse().map_err(failure::Error::from))
+                    }
+                }
+                Err(err) => Some(Err(err.into())),
             })
             .collect::<Result<Vec<EventMarker>, _>>()?;
         unimplemented!()
@@ -54,6 +64,13 @@ impl Synchro {
 impl FromStr for EventMarker {
     type Err = Error;
     fn from_str(s: &str) -> Result<EventMarker, Error> {
+        use regex::Regex;
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^(?P<date>\d{4}/\d{2}/\d{2})\s+(?P<time>\d{2}:\d{2}:\d{2}.\d{4})\s+(?P<number>\d+)$").unwrap();
+        }
+        let captures = RE
+            .captures(s)
+            .ok_or(Error::InvalidEventMarker(s.to_string()))?;
         unimplemented!()
     }
 }
